62319b7e25bf5eaf6afff3df903d3567d086d206
Unknown <felix.sabatie@gmail.com>
2018-10-25 12:48:14 -0400

    Reservation div basics

--- front/src/views/lift/ShowLift.vue
+++ front/src/views/lift/ShowLift.vue
@@ -5,60 +5,72 @@
     </div>
     <div v-else-if="lift !== undefined" class="lift-container">
       <h2>Trajet de {{lift.departure_city}} à {{lift.arrival_city}}</h2>
-      <div class="row">
-        <div class="col m3 s12 label">
-          Adresse de départ
+      <div class="infos">
+        <div class="row">
+          <div class="col m3 s12 label">
+            Adresse de départ
+          </div>
+          <div class="col m9 s11 offset-s1 icon-text">
+            <i class="material-icons prefix">place</i>
+            <span>{{lift.departure_address}}, {{lift.departure_city}}</span>
+          </div>
         </div>
-        <div class="col m9 s11 offset-s1 icon-text">
-          <i class="material-icons prefix">place</i>
-          <span>{{lift.departure_address}}, {{lift.departure_city}}</span>
+
+        <div class="row">
+          <div class="col m3 s12 label">
+            Adresse d'arrivée
+          </div>
+          <div class="col m9 s11 offset-s1 icon-text">
+            <i class="material-icons prefix">flag</i>
+            <span>{{lift.arrival_address}}, {{lift.arrival_city}}</span>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col m3 s12 label">
+            Date de départ
+          </div>
+          <div class="col m9 s11 offset-s1 icon-text">
+            <i class="material-icons prefix">date_range</i>
+            <span>{{formatDate(lift.departure_date)}}</span>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col m3 s12 label">
+            Date d'arrivée
+          </div>
+          <div class="col m9 s11 offset-s1 icon-text">
+            <i class="material-icons prefix">date_range</i>
+            <span>{{formatDate(lift.arrival_date)}}</span>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col m3 s12 label">
+            Véhicule
+          </div>
+          <div class="col m9 s11 offset-s1 icon-text">
+            <i class="material-icons prefix">directions_car</i>
+            <span>{{lift.car}}</span>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col s12 label">
+            {{nbPlacesLeft}} places restantes
+          </div>
         </div>
       </div>
-
-      <div class="row">
-        <div class="col m3 s12 label">
-          Adresse d'arrivée
+      <div class="reservation">
+        <div class="price">Prix par place : <span class="price-number">${{lift.price}}</span></div>
+        <div class="input-field">
+          <select v-model="nbPlaces">
+            <option v-for="i in (1, nbPlacesLeft)" :value="i">{{i}} place{{i > 1 ? 's' : ''}}</option>
+          </select>
+          <label>Nombre de places</label>
         </div>
-        <div class="col m9 s11 offset-s1 icon-text">
-          <i class="material-icons prefix">flag</i>
-          <span>{{lift.arrival_address}}, {{lift.arrival_city}}</span>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col m3 s12 label">
-          Date de départ
-        </div>
-        <div class="col m9 s11 offset-s1 icon-text">
-          <i class="material-icons prefix">date_range</i>
-          <span>{{formatDate(lift.departure_date)}}</span>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col m3 s12 label">
-          Date d'arrivée
-        </div>
-        <div class="col m9 s11 offset-s1 icon-text">
-          <i class="material-icons prefix">date_range</i>
-          <span>{{formatDate(lift.arrival_date)}}</span>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col m3 s12 label">
-          Véhicule
-        </div>
-        <div class="col m9 s11 offset-s1 icon-text">
-          <i class="material-icons prefix">directions_car</i>
-          <span>{{lift.car}}</span>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col s12 label">
-          {{nbPlacesLeft}} places restantes
-        </div>
+        <button class="btn">Réserver</button>
       </div>
     </div>
     <div v-else>
@@ -73,12 +85,14 @@
   import Unfound from '../Unfound'
   import Loader from '../Loader'
   import moment from 'moment'
+  import M from 'materialize-css'
 
   export default {
     data() {
       return {
         lift: undefined,
-        loading: true
+        loading: true,
+        nbPlaces: 1
       }
     },
     computed: {
@@ -92,8 +106,14 @@
       let liftId = Number(this.$route.params.id)
       if(!isNaN(liftId)) {
         axios.get(`${serverAddress}/api/lifts/${liftId}`).then(response => {
-          this.lift = response.data
+          this.lift = {
+            ...response.data,
+            price: parseFloat(response.data.price)
+          }
           this.loading = false
+          this.$nextTick(() => {
+            this.initSelect()
+          })
         }).catch(error => {
           this.loading = false
         })
@@ -104,6 +124,10 @@
     methods: {
       formatDate(date) {
         return moment(date).format('[Le ]DD/MM/YYYY[, à ]HH[h]mm')
+      },
+      initSelect() {
+        let elems = document.querySelectorAll('select');
+        M.FormSelect.init(elems);
       }
     }
   }
