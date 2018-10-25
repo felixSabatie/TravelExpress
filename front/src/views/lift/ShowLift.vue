930295e37e781d6b5e836cd3f5e0d47ad69d9da5
Unknown <felix.sabatie@gmail.com>
2018-10-25 12:27:14 -0400

    Show basic infos

--- front/src/views/lift/ShowLift.vue
+++ front/src/views/lift/ShowLift.vue
@@ -3,8 +3,63 @@
     <div v-if="loading">
       <Loader />
     </div>
-    <div v-else-if="lift !== undefined">
-      <h2>Hello lift</h2>
+    <div v-else-if="lift !== undefined" class="lift-container">
+      <h2>Trajet de {{lift.departure_city}} à {{lift.arrival_city}}</h2>
+      <div class="row">
+        <div class="col m3 s12 label">
+          Adresse de départ
+        </div>
+        <div class="col m9 s11 offset-s1 icon-text">
+          <i class="material-icons prefix">place</i>
+          <span>{{lift.departure_address}}, {{lift.departure_city}}</span>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col m3 s12 label">
+          Adresse d'arrivée
+        </div>
+        <div class="col m9 s11 offset-s1 icon-text">
+          <i class="material-icons prefix">flag</i>
+          <span>{{lift.arrival_address}}, {{lift.arrival_city}}</span>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col m3 s12 label">
+          Date de départ
+        </div>
+        <div class="col m9 s11 offset-s1 icon-text">
+          <i class="material-icons prefix">date_range</i>
+          <span>{{formatDate(lift.departure_date)}}</span>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col m3 s12 label">
+          Date d'arrivée
+        </div>
+        <div class="col m9 s11 offset-s1 icon-text">
+          <i class="material-icons prefix">date_range</i>
+          <span>{{formatDate(lift.arrival_date)}}</span>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col m3 s12 label">
+          Véhicule
+        </div>
+        <div class="col m9 s11 offset-s1 icon-text">
+          <i class="material-icons prefix">directions_car</i>
+          <span>{{lift.car}}</span>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col s12 label">
+          {{nbPlacesLeft}} places restantes
+        </div>
+      </div>
     </div>
     <div v-else>
       <Unfound />
@@ -17,12 +72,19 @@
   import {serverAddress} from '../../env'
   import Unfound from '../Unfound'
   import Loader from '../Loader'
+  import moment from 'moment'
 
   export default {
     data() {
       return {
         lift: undefined,
         loading: true
+      }
+    },
+    computed: {
+      nbPlacesLeft() {
+        if(this.lift !== undefined)
+          return this.lift.capacity - this.lift.passengers.length
       }
     },
     components: { Unfound, Loader },
@@ -38,10 +100,29 @@
       } else {
         this.loading = false
       }
+    },
+    methods: {
+      formatDate(date) {
+        return moment(date).format('[Le ]DD/MM/YYYY[, à ]HH[h]mm')
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  @import '../../styles/colors';
 
+  .lift-container {
+    .label {
+      color: $grey-text-color;
+      font-weight: bold;
+    }
+    .icon-text {
+      display: flex;
+      align-items: center;
+      span {
+        margin-left: 10px;
+      }
+    }
+  }
 </style>
