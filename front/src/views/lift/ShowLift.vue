595d00685f696fcc4b2bf594a5ba45e6e27520e8
Unknown <felix.sabatie@gmail.com>
2018-10-25 11:26:39 -0400

    Loading and 404

--- front/src/views/lift/ShowLift.vue
+++ front/src/views/lift/ShowLift.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="show-lift">
-    <h2>Hello lift</h2>
+    <div v-if="loading">
+      <Loader />
+    </div>
+    <div v-else-if="lift !== undefined">
+      <h2>Hello lift</h2>
+    </div>
+    <div v-else>
+      <Unfound />
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
+  import axios from 'axios'
+  import {serverAddress} from '../../env'
+  import Unfound from '../Unfound'
+  import Loader from '../Loader'
 
+  export default {
+    data() {
+      return {
+        lift: undefined,
+        loading: true
+      }
+    },
+    components: { Unfound, Loader },
+    mounted() {
+      let liftId = Number(this.$route.params.id)
+      if(!isNaN(liftId)) {
+        axios.get(`${serverAddress}/api/lifts/${liftId}`).then(response => {
+          this.lift = response.data
+          this.loading = false
+        }).catch(error => {
+          this.loading = false
+        })
+      } else {
+        this.loading = false
+      }
+    }
   }
 </script>
 
