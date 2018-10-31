5bb0b252fd4c0685ff58f123a53877acb7337bf1
SimonMarchand <simonmarchand@outlook.com>
2018-10-31 15:36:49 -0400

    Back controller and model #10

--- app/controllers/api/accounts_controller.rb
+++ app/controllers/api/accounts_controller.rb
@@ -1,4 +1,34 @@
 module Api
   class AccountsController < ApplicationController
+
+    before_action :set_account, only: [:show]
+
+    def show
+      render_json_with_includes(@account)
+    end
+
+    def create
+      account = Account.new(account_params)
+      if account.save
+        render_json_with_includes account
+      else
+        render status: 422, json: {error: 'Account is invalid'}
+      end
+    end
+
+    private
+
+    def set_account
+      @account = Account.includes([:drivers, :passengers]).find(params[:id])
+    end
+
+    def render_json_with_includes(data)
+      render json: data, include: [:drivers, :passengers]
+    end
+
+    def account_params
+      params.require(:account).permit(:first_name, :last_name, :email, :password)
+    end
+
   end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,14 @@
 class Account < ApplicationRecord
   has_and_belongs_to_many :drivers, :class_name => 'Lift', :join_table => 'drivers'
   has_and_belongs_to_many :passengers, :class_name => 'Lift', :join_table => 'passengers'
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :email, presence: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+
+  validates :password, presence: true
+  validates_confirmation_of :password
+  validates_presence_of :password_confirmation, if: :password_changed?
 end
